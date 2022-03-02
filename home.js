12209f5e53ce33a163507d457171497bcea0b5de
Aishwarya Lakshmi <64244620+aishwarya-15@users.noreply.github.com>
2022-03-02 10:08:10 +0530

    Add files via upload

--- home.js
+++ home.js
@@ -1,89 +1,113 @@
-document.addEventListener('DOMContentLoaded',loadBooksUI);
-document.addEventListener('DOMContentLoaded',loadUI);
+document.addEventListener('DOMContentLoaded', loadBooksUI);
+document.addEventListener('DOMContentLoaded', loadUI);
+document.addEventListener('DOMContentLoaded', displayBooksInOrdersUI);
 
-function loadUI(){
+function loadUI() {
   let loggedUser = JSON.parse(localStorage.getItem('loggedUser'));
-  if(loggedUser.role){
+  if (loggedUser.role) {
     let userElements = document.querySelectorAll('.user');
     userElements.forEach(item => item.remove());
   }
-  else{
+  else {
     let adminElements = document.querySelectorAll('.admin');
     adminElements.forEach(item => item.remove());
   }
 }
-function Book(title,author,description,price,quantity,image){
-  this.title=title;
-  this.author=author;
-  this.description=description;
-  this.price=price;
-  this.quantity =quantity;
-  this.image=image;
+
+function Book(title, author, description, price, quantity, image) {
+  this.title = title;
+  this.author = author;
+  this.description = description;
+  this.price = price;
+  this.quantity = quantity;
+  this.image = image;
 }
 
-function loadBooksUI(){
- 
+function loadBooksUI() {
+
   let booksDiv = document.querySelector('.shop-items');
-  booksDiv.innerHTML="";
+  booksDiv.innerHTML = "";
   let books = JSON.parse(localStorage.getItem('bookStorage'));
-
-  for(let book of books){
-    let bookItem = document.createElement('div');
-    let imgSrc = book.image || "/home/elcot/Downloads/book.jpeg"
-    bookItem.innerHTML = `
-    <span class="shop-item-title">${book.title}</span>
-    <img class="shop-item-image" src=${imgSrc}>
-    <div class="shop-item-details">
-        <span class="shop-item-description">${book.description}</span>
-        <span class="shop-item-author">${book.author}</span>
-        <span class="shop-item-price">Rs.${book.price}</span>
-        <section class="section-quantity">  
-            <button class="shop-item-decrease" type="button">-</button>
-            <span class="shop-item-quantity admin">${book.quantity}</span>
-            <span class="shop-item-quantity user">0</span>
-            <button class="shop-item-increase" type="button">+</button>
-        </section>
-        <button class="btn-delete admin" type="button">Delete</button>
-        <button class="btn-buy user" type="button">Buy</button>
-    </div>
-    `;
-    bookItem.classList.add('shop-item');
-    booksDiv.appendChild(bookItem);
-  } 
+  let loggedUser = (JSON.parse(localStorage.getItem('loggedUser'))).role;
+  // console.log(loggedUser);
+  if (loggedUser) {
+    for (let book of books) {
+      let bookItem = document.createElement('div');
+      let imgSrc = book.image || "/home/elcot/Downloads/book.jpeg"
+      bookItem.innerHTML = `
+      <span class="shop-item-title">${book.title}</span>
+      <img class="shop-item-image" src=${imgSrc}>
+      <div class="shop-item-details">
+          <span class="shop-item-description">${book.description}</span>
+          <span class="shop-item-author">${book.author}</span>
+          <span class="shop-item-price">Rs.${book.price}</span>
+          <section class="section-quantity">  
+              <button class="shop-item-decrease" type="button">-</button>
+              <span class="shop-item-quantity admin">${book.quantity}</span>
+              <button class="shop-item-increase" type="button">+</button>
+          </section>
+          <button class="btn-delete" type="button">Delete</button>
+      </div>
+      `;
+      bookItem.classList.add('shop-item');
+      booksDiv.appendChild(bookItem);
+    }
+  }
+  else {
+    for (let book of books) {
+      let bookItem = document.createElement('div');
+      let imgSrc = book.image || "/home/elcot/Downloads/book.jpeg"
+      bookItem.innerHTML = `
+        <span class="shop-item-title">${book.title}</span>
+        <img class="shop-item-image" src=${imgSrc}>
+        <div class="shop-item-details">
+            <span class="shop-item-description">${book.description}</span>
+            <span class="shop-item-author">${book.author}</span>
+            <span class="shop-item-price">Rs.${book.price}</span>
+            <section class="section-quantity">  
+              <button class="shop-item-decrease" type="button">-</button>
+              <span class="shop-item-quantity user">0</span>
+              <button class="shop-item-increase" type="button">+</button>
+          </section>
+            <button class="btn-buy user" type="button">Buy</button>
+        </div>
+        `;
+      bookItem.classList.add('shop-item');
+      booksDiv.appendChild(bookItem);
+    }
+  }
 }
 
-
-
-function Logout(){
+function Logout() {
   localStorage.removeItem('loggedUser');
-  return window.location.href="/home/elcot/BookStore/index.html";
+  return window.location.href = "/home/elcot/BookStore/index.html";
 }
 
-function openModal(){
+function openModal() {
   document.querySelector('.modal-bg').classList.remove('hidden');
 }
 
-function closeModal(e){
-  if(e.target.tagName === "I")
+function closeModal(e) {
+  if (e.target.tagName === "I")
     document.querySelector('.modal-bg').classList.add('hidden');
-  if(e.target.tagName === "BUTTON"){
+  if (e.target.tagName === "BUTTON") {
     let form = document.querySelector('.form-group');
-    let inputs= form.querySelectorAll('input');
+    let inputs = form.querySelectorAll('input');
     let values = [];
-    for(let input of inputs){
+    for (let input of inputs) {
       values.push(input.value);
       input.value = "";
     }
-    let book = new Book(values[0],values[1],values[2],values[3],values[4],values[5]);
+    let book = new Book(values[0], values[1], values[2], values[3], values[4], values[5]);
     let bookStorage;
     // if(localStorage.getItem('bookStorage')===null){
     //   bookStorage = [book];
     // }else{
-      bookStorage = JSON.parse(localStorage.getItem('bookStorage'));
-      bookStorage.push(book);
+    bookStorage = JSON.parse(localStorage.getItem('bookStorage'));
+    bookStorage.push(book);
     // }
-     localStorage.setItem('bookStorage',JSON.stringify(bookStorage));
-     document.querySelector('.modal-bg').classList.add('hidden');
+    localStorage.setItem('bookStorage', JSON.stringify(bookStorage));
+    document.querySelector('.modal-bg').classList.add('hidden');
     //  alert("Book Added Successfully")
     loadBooksUI();
     loadUI();
@@ -100,130 +124,190 @@ function openPage(pageName) {
 }
 
 
-function searchFilter(e){
-    let term = e.target.value.toLowerCase();
-    let titles = document.querySelectorAll('.shop-item-title');
-    let shopItems = document.querySelectorAll('.shop-item');
-    titles.forEach(
-        (title) => {
-            if(title.innerText.toLowerCase().indexOf(term) != -1){
-                title.closest('.shop-item').style.display="block";
-            }
-            else{
-                title.closest('.shop-item').style.display="none";
-            }
-        }
-    )
-    // for(let title of titles){
-    //      if(title.innerText.toLowerCase().indexOf(term) !== -1){
-    //           title.closest('.shop-item').style.display="block";
-    //      }
-    //      else{
-    //         title.closest('.shop-item').style.display="none";
-    //      }
-    // }
+function searchFilter(e) {
+  let term = e.target.value.toLowerCase();
+  let titles = document.querySelectorAll('.shop-item-title');
+  let shopItems = document.querySelectorAll('.shop-item');
+  titles.forEach(
+    (title) => {
+      if (title.innerText.toLowerCase().indexOf(term) != -1) {
+        title.closest('.shop-item').style.display = "block";
+      }
+      else {
+        title.closest('.shop-item').style.display = "none";
+      }
+    }
+  )
 }
-  
-function deleteBookUI(el){
-  if(el.srcElement.innerText ==='Delete'){
+
+function deleteBookUI(el) {
+  if (el.srcElement.innerText === 'Delete') {
     let shopItem = el.toElement.parentElement.parentElement;
     let title = shopItem.children[0].innerText;
     removeBook(title);
-    shopItem.remove();  
+    shopItem.remove();
   }
   else return;
 }
 
-function removeBook(title){
-   let books = JSON.parse(localStorage.getItem('bookStorage'));
-
-   books.forEach((book,index) => {
-      if(book.title === title){
-        books.splice(index,1);
-      }else{
-        console.log(book.title)
-      }
-   });
-   localStorage.setItem('bookStorage',JSON.stringify(books));
-}
-
-function updateQtyInStorage(title,qty){
+function removeBook(title) {
   let books = JSON.parse(localStorage.getItem('bookStorage'));
 
-  books.forEach((book) => {
-     if(book.title === title){
-       book.quantity = qty;
-     }
+  books.forEach((book, index) => {
+    if (book.title === title) {
+      books.splice(index, 1);
+    } else {
+      console.log(book.title)
+    }
   });
-  localStorage.setItem('bookStorage',JSON.stringify(books));
+  localStorage.setItem('bookStorage', JSON.stringify(books));
 }
 
-function incrementQty(e){
+function updateQtyInStorage(title, qty) {
+  let books = JSON.parse(localStorage.getItem('bookStorage'));
+  if (JSON.parse(localStorage.getItem('loggedUser')).role) {
+    books.forEach((book) => {
+      if (book.title === title) {
+        book.quantity = qty;
+      }
+    });
+  } else {
+    books.forEach((book) => {
+      if (book.title === title) {
+        book.quantity -= qty;
+      }
+    });
+  }
+  localStorage.setItem('bookStorage', JSON.stringify(books));
+}
+
+function incrementQty(e) {
   let qty = e.target.previousElementSibling;
-  if(qty.classList.value ==="shop-item-quantity admin"){
-          qty.innerText++;
+  if (qty.classList.value === "shop-item-quantity admin") {
+    qty.innerText++;
+    let item = qty.closest('.shop-item');
+    updateQtyInStorage(item.children[0].innerText, Number(qty.innerText));
   }
-  let item = qty.closest('.shop-item');
-  updateQtyInStorage(item.children[0].innerText,qty.innerText);
-
+  else if (qty.classList.value === "shop-item-quantity user") {
+    qty.innerText++;
+    // console.log(Number(qty.innerText));
+  } else return;
 }
 
-function decrementQty(e){
+function decrementQty(e) {
   let qty = e.target.nextElementSibling;
-  if(qty.classList.value ==="shop-item-quantity admin" && qty.innerText>0){
-      qty.innerText--;
+  if (qty.classList.value === "shop-item-quantity admin" && qty.innerText > 0) {
+    qty.innerText--;
+    let item = qty.closest('.shop-item');
+    updateQtyInStorage(item.children[0].innerText, Number(qty.innerText), "dec");
+  } else if (qty.classList.value === "shop-item-quantity user" && qty.innerText > 0) {
+    qty.innerText--;
+  } else return;
+}
+
+
+function Order(orderId, username, email, title, qty, date, total) {
+  this.orderId = orderId;
+  this.username = username;
+  this.email = email;
+  this.title = title;
+  this.quantity = qty;
+  this.date = date;
+  this.total = total;
+}
+
+function orderStorage(orderId, username, email, title, quantity, date, total) {
+  let newOrder = new Order(orderId, username, email, title, quantity, date, total);
+  let storage = JSON.parse(localStorage.getItem('orderStorage'));
+  storage.push(newOrder);
+  localStorage.setItem('orderStorage', JSON.stringify(storage));
+}
+
+function purchase(e) {
+  if (e.target.classList.contains("btn-buy")) {
+    let title = e.target.closest('.shop-item').children[0].innerText;
+    // let price = e.target.closest('.shop-item').children[2];
+    let price = e.target.previousElementSibling.previousElementSibling.innerText;
+    // console.log(price);
+    price = price.slice(3);
+    let qty = e.target.previousElementSibling.children[1].innerText;
+    // console.log(qty)
+    updateQtyInStorage(title, qty);
+    let total = price * qty;
+    let loggedUser = JSON.parse(localStorage.getItem('loggedUser'));
+    let username = loggedUser.username;
+    let email = loggedUser.email;
+    let date = Date.now();
+    let orderId = localStorage.getItem('orderId');
+    // console.log(orderId)
+    localStorage.setItem('orderId', ++orderId);
+
+    orderStorage(orderId, username, email, title, qty, date, total);
+    displayBooksInOrdersUI();
   }
-  let item = qty.closest('.shop-item');
-  updateQtyInStorage(item.children[0].innerText,qty.innerText);
+  else return;
 }
 
+function displayBooksInOrdersUI() {
 
-function addToCart(e){
-   if(e.target.classList.value ==="btn-buy user"){
+  let orders = JSON.parse(localStorage.getItem('orderStorage'));
+  let table = document.querySelector('.table');
+  table.innerHTML = "";
+  let head = document.createElement('tr');
+  head.innerHTML = `
+  <th>Order Id</th>
+  <th>Buyer Email</th>
+  <th>Book Name</th>
+  <th>Quantity</th>
+  <th>Order date</th>
+  <th>Total amount</th>`;
+  table.appendChild(head);
+
+
+  let loggedUser = JSON.parse(localStorage.getItem('loggedUser'));
+  if (loggedUser.role) {
+    for (let order of orders) {
+      let row = document.createElement('tr');
+      row.innerHTML = `
+      <td>${order.orderId}</td>
+      <td>${order.email}</td>
+      <td>${order.title}</td>
+      <td>${order.quantity}</td>
+      <td>${Date(order.date)}</td>
+      <td>${order.total}</td>`
+      // console.log(row);
+      table.appendChild(row);
+    }
+  }
+  else {
+    for (let order of orders) {
+      if (order.username === loggedUser.username) {
         let row = document.createElement('tr');
-        row.innerHTML =`
-        <td>{title}</td>
-        <td>price</td>
-        <td>A</td>
-    `
-    console.log(row);
-    // let cart = document.querySelector('.cart');
-    let cartTotal = document.querySelector('.cart-total');
-    // console.log(cartTotal)
-    cart.insertBefore(row,cartTotal);
-    // cart.appendChild(row);
-   }
+        row.innerHTML = `
+        <td>${order.orderId}</td>
+        <td>${order.email}</td>
+        <td>${order.title}</td>
+        <td>${order.quantity}</td>
+        <td>${Date(order.date)}</td>
+        <td>${order.total}</td>`
+        // console.log(row);
+        table.appendChild(row);
+      }
+    }
+  }
 }
-
-
-
-
-
-
-
-
-
-
-
 let logout = document.getElementById('logout');
 let addbook = document.getElementById('add-book');
 let modal = document.querySelector('.addbook-modal');
 let searchBar = document.getElementById('search');
 // let deleteBtn= document.querySelector('.btn-delete'); 
-let items =document.querySelector('.shop-items');
-
-
-
-
+let items = document.querySelector('.shop-items');
 document.getElementById("defaultOpen").click();
-logout.addEventListener('click',Logout);  
-addbook.addEventListener('click',openModal);
-modal.addEventListener('click',closeModal);
-searchBar.addEventListener('keyup',searchFilter);
-items.addEventListener('click',deleteBookUI);
-items.addEventListener('click',incrementQty);
-items.addEventListener('click',decrementQty);
-items.addEventListener('click',addToCart);
-
-
-
+logout.addEventListener('click', Logout);
+addbook.addEventListener('click', openModal);
+modal.addEventListener('click', closeModal);
+searchBar.addEventListener('keyup', searchFilter);
+items.addEventListener('click', deleteBookUI);
+items.addEventListener('click', incrementQty);
+items.addEventListener('click', decrementQty);
+items.addEventListener('click', purchase);
